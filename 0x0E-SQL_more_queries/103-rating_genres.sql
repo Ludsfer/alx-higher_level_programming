845e999063c4449d42b4aae8fd96c23e544decbc
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2023-12-14 05:08:40 +0000

    Completed

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,12 @@
+-- A script that lists all genres in the database hbtn_0d_tvshows_rate by their rating.
+SELECT
+	tg.name,
+	SUM(tsr.rate) AS `rating`
+FROM
+	tv_genres AS tg
+	LEFT JOIN tv_show_genres AS tsg ON tg.id = tsg.genre_id
+	LEFT JOIN tv_show_ratings AS tsr ON tsr.show_id = tsg.show_id
+GROUP BY
+	tg.name
+ORDER BY
+	SUM(tsr.rate) DESC;
