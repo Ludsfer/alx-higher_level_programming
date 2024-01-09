110421a35122669a4b04b2ac23528e9403afdacf
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-09 04:55:34 +0000

    Completed

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length <= 3) {
-	console.log(0);
+  console.log('0');
 } else {
-	const argument = process.argv.map(Number)
-		.slice(2, process.argv.length)
-		.sort((a, b) => a - b);
-	console.log(argument[argument.length - 2]);
-}}
+  const arr = process.argv.slice(2).map(Number);
+  const second = arr.sort(function (a, b) { return b - a; })[1];
+  console.log(second);
+}
