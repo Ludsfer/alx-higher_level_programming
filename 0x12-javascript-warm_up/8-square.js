f68db4bc3aff651f56f693ebafce11c6d1c61dec
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-09 04:44:09 +0000

    Completed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,15 +2,15 @@
 
 const argument = process.argv;
 if (!isNaN(Number(argument[2]))) {
-	let sampleString = '';
-	for (let index = 0; index < Number(argument[2]); index++) {
-		sampleString = sampleString + 'X';
-	}
-	let i = 0;
-	while (i < Number(argument[2])) {
-		console.log(sampleString);
-		i++;
-	}
+  let sampleString = '';
+  for (let index = 0; index < Number(argument[2]); index++) {
+    sampleString = sampleString + 'X';
+  }
+  let i = 0;
+  while (i < Number(argument[2])) {
+    console.log(sampleString);
+    i++;
+  }
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
