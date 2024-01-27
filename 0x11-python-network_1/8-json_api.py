e9e3d2604f8d129d1bb1f43f6bbf9856c1a7cb3c
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-27 04:21:17 +0000

    Completed

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Sends a POST request to a URL with a given letter to
+http://0.0.0.0:5000/search_user and displays the response body
+"""
+if __name__ == '__main__':
+    from sys import argv
+    import requests
+
+    url = "http://0.0.0.0:5000/search_user"
+    try:
+        data = {'q': argv[1]}
+    except IndexError:
+        data = {'q': ''}
+    res = requests.post(url, data=data)
+
+    try:
+        json_str = res.json()
+        if len(json_str) == 0:
+            print('No result')
+        else:
+            print(f"[{json_str['id']}] {json_str['name']}")
+    except Exception:
+        print('Not a valid JSON')
