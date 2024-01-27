c903fb0fb522b3f6b7e96aab0b9e8af35e2e7b2f
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-27 07:22:53 +0000

    Completed

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-"""Finds a peak in a list of unsorted integers"""
+"""A script that finds a peak in a list of unsorted integers"""
 
 
 def find_peak(list_of_integers):
-    """Finds a peak in a list_of_integers"""
+    """Defines the function peak"""
 
     if list_of_integers is None or list_of_integers == []:
         return None
@@ -22,3 +22,11 @@ def find_peak(list_of_integers):
         return find_peak(list_of_integers[mid:])
     if mid > 0 and list_of_integers[mid] < list_of_integers[mid - 1]:
         return find_peak(list_of_integers[:mid])
+
+
+print(find_peak([1, 2, 4, 6, 3]))
+print(find_peak([4, 2, 1, 2, 3, 1]))
+print(find_peak([2, 2, 2]))
+print(find_peak([]))
+print(find_peak([-2, -4, 2, 1]))
+print(find_peak([4, 2, 1, 2, 2, 2, 3, 1]))
