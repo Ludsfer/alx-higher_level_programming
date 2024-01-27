b3badc1484b7fd60fc80a0f6772fbf4b712673c3
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-27 04:15:25 +0000

    Completed

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# send a GET request to the URL. A header variable X-School-User-Id must be sent with the value 98
+# send a GET request to URL. A header variable X-School-User-Id sent with the value 98
 curl -sH "X-HolbertonSchool-User-Id: 98" "${1}
