429683a5a1ebe3b756bc899707385cd1f3d9c18f
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-27 04:35:29 +0000

    Completed

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """Python script that takes your GitHub credentials (username and password)
 and uses the GitHub API to display your id"""
-import requests
-from requests.auth import HTTPBasicAuth
-import sys
-
-
-def searchapi():
-    """status"""
+if __name__ == "__main__":
+    import requests
+    from requests.auth import HTTPBasicAuth
+    import sys
+    
     user = str(sys.argv[1])
     pw = str(sys.argv[2])
     result = requests.get("https://api.github.com/user",
@@ -19,5 +17,3 @@ def searchapi():
     except:
         print("None")
 
-if __name__ == "__main__":
-    searchapi()
