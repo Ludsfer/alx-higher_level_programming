a78c943586acf755b2b22355f494cf33c09fd112
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-27 04:11:20 +0000

    Completed

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sends a request to a URL and displays the value of the X-Request-Id
+in the header"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    email = argv[2]
+    url = argv[1]
+    data = {
+        'email': email
+    }
+
+    res = requests.post(url, data=data)
+    print(res.text)
