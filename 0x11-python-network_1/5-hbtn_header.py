3c9924f14fd4d9d463979182a0bae29cb4b67175
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-01-27 04:08:35 +0000

    Completed

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Send a request to a URL and display the header X-Request-Id"""
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    res = requests.get(argv[1])
+    print(res.headers.get('X-Request-Id'))
