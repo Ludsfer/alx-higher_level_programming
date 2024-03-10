f48c8c99009c4714de790e06e3d0245bc21c0362
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-03-10 00:26:56 +0000

    Completed

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+A script that implements a function to write to a file.
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    Writes a string to a text file (UTF8) and returns the number of characters written
+
+    Args:
+        filename: Name of the file
+        text:     The string to be written to file
+    """
+    with open(filename, mode='w', encoding='utf-8') as txt_file:
+        char_count = txt_file.write(text)
+    return char_count
