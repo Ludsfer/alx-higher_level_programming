f1739731dd7c801e778fccd467aa263a4f5321ae
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-03-10 00:51:46 +0000

    Completed

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+A module that implements a function to creates an object
+from a JSON file.
+"""
+
+
+def load_from_json_file(filename: str) -> object:
+    """
+    Create an object from a JSON file
+
+    Args:
+        filename: Name of the file to write to.
+    """
+    import json
+
+    with open(filename, mode='r', encoding='utf-8') as json_file:
+        return json.load(json_file)
