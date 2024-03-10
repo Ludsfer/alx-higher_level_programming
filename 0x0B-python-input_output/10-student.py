ec37e366a9ba494aebf6bf5cd8fbeaaa4222da5f
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-03-10 01:21:40 +0000

    Completed

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" 
+A module that implements a Student class
+"""
+
+
+class Student:
+    
+    def __init__(self, first_name, last_name, age):
+        """
+        Student class constructor
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        A method that returns directory description
+        """
+        obj = self.__dict__.copy()
+        if type(attrs) is list:
+
+            for item in attrs:
+                if type(item) is not str:
+                    return obj
+
+            d_list = {}
+
+            for iatr in range(len(attrs)):
+                for satr in obj:
+                    if attrs[iatr] == satr:
+                        d_list[satr] = obj[satr]
+            return d_list
+
+        return obj
