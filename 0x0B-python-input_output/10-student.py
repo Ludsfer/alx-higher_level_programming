4aa25016f828c6295519a6e42f6d3d06dccc170d
Ludsfer <53081901+Ludsfer@users.noreply.github.com>
2024-03-10 01:23:20 +0000

    Completed

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-""" 
+"""
 A module that implements a Student class
 """
 
 
 class Student:
-    
+
     def __init__(self, first_name, last_name, age):
         """
         Student class constructor
